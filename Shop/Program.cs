a33abce276ec071f6a44ff546e58e7a9eb6b392e
byrilovavika-a11y <byrilovavika@gmail.com>
2025-12-27 08:25:06 +0500

    Add files via upload

---
+++ Shop/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Shop
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+
+            string conStr = ConfigurationManager.ConnectionStrings["DeafultCon"].ConnectionString;
+
+            MessageBox.Show(conStr);
+        }
+    }
+}
